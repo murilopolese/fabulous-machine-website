af9b4b1b939ad38f6da6aa27f45fabec47af3cd7
Murilo Polese <murilopolese@gmail.com>
2017-02-23 19:15:47 +0000

    Increase payload for body-parser

--- index.js
+++ index.js
@@ -27,10 +27,8 @@ var app = express();
 app.set( 'view engine', 'hbs' );
 app.set( 'views', __dirname + '/views' );
 app.use( express.static( __dirname + '/public' ) );
-app.use( bodyParser.json() );
-app.use( bodyParser.urlencoded({
-  extended: true
-}));
+app.use(bodyParser.json({limit: '5mb'}));
+app.use(bodyParser.urlencoded({limit: '5mb',extended: true}));
 
 app.get( '/', function( req, res ) {
     res.render( 'home' );
